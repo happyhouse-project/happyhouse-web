2a134610f328165ce665cd391a64caf4dfd17c98
JUSANG <wntkd0530@naver.com>
2020-11-24 23:00:04 +0900

    [디자인] #게시글 디자인 수정

--- src/views/NoticeDetail.vue
+++ src/views/NoticeDetail.vue
@@ -1,19 +1,24 @@
 <template>
    <div class="container">
-      <div class="detail-warpper shadow-lg bg-white">
-         <div class="detail-title">
-            {{ notice.id }}
-            <h3>{{ notice.title }}</h3>
+      <div class="detail-warpper shadow-lg">
+         <div class="detail-font detail-title mx-auto text-center">
+            <h1>{{ notice.title }}</h1>
          </div>
-         <div class="detail-info">
-            <span class="writer"> {{ notice.writer }}</span>
-            <span class="writeDate"> {{ notice.writeDate }}</span>
+         <div class="detail-info my-3 text-center">
+            <span class="writer">
+               <kbd style="margin-right:10px">{{ notice.writer }}</kbd></span
+            >
+            <span class="writeDate">
+               <pre style="display:inline">{{ notice.writeDate }}</pre></span
+            >
             <!-- <span>{{hit}}</span> -->
          </div>
-         <div class="detail-content">
-            <textarea readonly cols="120" rows="20" v-model="notice.content"></textarea>
+         <div class="detail-content shadow-lg bg-white">
+            <textarea readonly class="w-100" rows="15" v-model="notice.content"></textarea>
          </div>
-         <button type="button" @click="remove(notice.id)">삭제하기</button>&nbsp;&nbsp;&nbsp;
+         <div class="w-100 text-center mt-4">
+            <b-button size="md" pill variant="outline-danger" @click="remove(notice.id)"><span style="padding: 0px 30px">delete</span></b-button>
+         </div>
       </div>
    </div>
 </template>
@@ -62,7 +67,34 @@ export default {
 </script>
 
 <style scoped>
+h1 {
+   font-family: 'Do Hyeon' !important;
+}
+.container {
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   font-family: 'Do Hyeon' !important;
+}
 .detail-warpper {
+   padding: 10px 10px 30px 10px;
+   border-radius: 20px;
    background-color: white;
+}
+
+.detail-content {
+   border-radius: 10px;
+   min-height: 300px;
+   height: 40vh;
+}
+.detail-content textarea {
+   border-radius: 10px;
+   min-height: 300px;
+   height: 40vh;
+   padding: 10px 20px 30px 20px;
+   border: 0;
+   font-size: 20pt;
+   color: rgb(70, 70, 70);
 }
 </style>
